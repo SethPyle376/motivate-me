850aa6c7237eca2814604e926cc6604de0cf684a
Seth Pyle <seth.pyle86@gmail.com>
2020-04-07 07:28:48 -0600

    work on board

---
+++ src/components/Board.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="test">
+        <v-card v-for="item in items" :key="item.message" class="d-inline-block mx-auto mr-6 mt-12">
+            <v-container class="ml-2">
+                <v-row justify="space-between">
+                    <v-cols cols="auto">
+                        <v-img
+                            max-height="500"
+                            max-width="500"
+                            src="https://giantbomb1.cbsistatic.com/uploads/scale_medium/1/12139/2754840-bethesda.jpg"/>
+                    </v-cols>
+                    <v-col cols="auto" class="text-center pl-0">
+                        <v-row class="flex-column ma-0 fill-height" justify="center">
+                            <v-col class="px-0">
+                                <v-btn icon>
+                                    <v-icon>mdi-heart</v-icon>
+                                </v-btn>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            items: [
+                {
+                    message: "hello"
+                },
+                {
+                    message: "world"
+                },
+                {
+                    message: "hello"
+                },
+                {
+                    message: "world"
+                },             
+                {
+                    message: "hello"
+                },
+                {
+                    message: "world"
+                }
+            ]
+        }
+    }
+}
+</script>
---
+++ src/router/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/board',
+    name: 'board',
+    component: () => import(/* webpackChunkName: "about" */ '../components/Board.vue'),
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
